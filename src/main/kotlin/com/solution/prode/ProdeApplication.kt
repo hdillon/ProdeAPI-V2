46c11dbfc6408ac7f04e2bef488e26d0f3d36504
Horacio Dillon <dillonhoraciodavid@gmail.com>
2019-12-30 20:46:56 -0300

    Project creation

---
+++ src/main/kotlin/com/solution/prode/ProdeApplication.kt
@@ -0,0 +1,11 @@
+package com.solution.prode
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProdeApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProdeApplication>(*args)
+}
