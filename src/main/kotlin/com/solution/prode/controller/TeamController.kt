8017a187ae1012e9f87c353a3f4aff4d8405d7ed
Horacio Dillon <dillonhoraciodavid@gmail.com>
2020-01-03 18:06:31 -0300

    Team controller functions

--- src/main/kotlin/com/solution/prode/controller/TeamController.kt
+++ src/main/kotlin/com/solution/prode/controller/TeamController.kt
@@ -1,15 +1,41 @@
 package com.solution.prode.controller
 
+import com.solution.prode.model.Team
 import com.solution.prode.repository.TeamRepository
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/api/v1/team")
 class TeamController(private val repository: TeamRepository) {
 
-    @GetMapping
+    @GetMapping("/all")
     fun findAll() = repository.findAll()
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Long) = repository.findById(id)
+
+    @PostMapping
+    fun create(@Valid @RequestBody newTeam: Team): Team =
+            repository.save(newTeam)
+
+    @PutMapping("/{id}")
+    fun updateById(@PathVariable id: Long, @Valid @RequestBody newTeam: Team): ResponseEntity<Team> {
+        return repository.findById(id).map { existingTeam ->
+            val updatedTeam: Team = existingTeam.copy(name = newTeam.name)
+            ResponseEntity.ok().body(repository.save(updatedTeam))
+        }.orElse(ResponseEntity.notFound().build())
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteById(@PathVariable(value = "id") id: Long): ResponseEntity<Void> {
+        return repository.findById(id).map { team  ->
+            repository.delete(team)
+            ResponseEntity<Void>(HttpStatus.OK)
+        }.orElse(ResponseEntity.notFound().build())
+
+    }
 
 }
--- src/main/kotlin/com/solution/prode/model/Team.kt
+++ src/main/kotlin/com/solution/prode/model/Team.kt
@@ -1,11 +1,13 @@
 package com.solution.prode.model
 
 import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
 import javax.persistence.Id
 
 @Entity
-class Team(
-        @Id
-        var id: String,
+data class Team(
+        @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+        var id: Long,
         var name: String
 )
